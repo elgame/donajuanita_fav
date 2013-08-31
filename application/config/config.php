76fc766ea81ad21939e99ce072c1e7c7aea605a8
elgame <gamalielmendozasolis@gmail.com>
2013-08-31 12:37:26 -0500

    dd

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/punto_venta/';
+$config['base_url']	= 'http://localhost/punto_venta_fav/';
 
 
 $config['vcarabiner']	= '1.2';
